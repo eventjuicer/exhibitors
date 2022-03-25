2c864e7ee8cc9f4b29c65a74f75070a6c0202b98
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-03-25 14:45:40 +0100

    added icon to ranking

--- src/layout/customRoutes.js
+++ src/layout/customRoutes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import AssignmentIcon from '@material-ui/icons/Assignment';
+import {Assignment, TrendingUp, VolumeUp} from '@material-ui/icons';
+
 
 
 import Configuration from '../views/configuration';
@@ -9,13 +10,18 @@ import Rewards from '../views/rewards';
 import Logistics from '../views/logistics'
 import Promote from '../views/promote'
 
+
+
+
 export default [
   <Route exact path="/configuration"><Configuration /></Route>,
-  <Route exact path="/rewards" component={Rewards} />,
-  <Route exact path="/ranking" component={Ranking} />,
-  <Route exact path="/promote" component={Promote} />,
+  <Route exact path="/ranking" icon={TrendingUp} component={Ranking} />,
+  <Route exact path="/promote" icon={VolumeUp} component={Promote} />,
+  <Route dupa="osiut" exact path="/logistics" icon={Assignment} component={Logistics} />,
 
+
+
+  <Route exact path="/rewards" component={Rewards} />,
   // <Route exact path="/badgescanner" component={ScannerAuth} />,
-  <Route dupa="osiut" exact path="/logistics" icon={AssignmentIcon} component={Logistics} />,
   <Route exact path="/invite-vip" component={Logistics} />
 ];
