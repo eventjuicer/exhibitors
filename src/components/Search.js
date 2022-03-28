f5470106e64ff869e146a8d763a0306c955f0657
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-03-29 00:11:13 +0200

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,16 +2,15 @@ import React, {useState, useEffect} from 'react';
 import { alpha, makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-
 import InputBase from '@material-ui/core/InputBase';
-import Badge from '@material-ui/core/Badge';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
 import SearchIcon from '@material-ui/icons/Search';
-import Button from '@material-ui/core/Button';
 import * as JsSearch from 'js-search';
 import { isFunction, isEmpty } from '../helpers';
 
+
+
+// import Badge from '@material-ui/core/Badge';
+// import Button from '@material-ui/core/Button';
 // import IconButton from '@material-ui/core/IconButton';
 // import Typography from '@material-ui/core/Typography';
 // import AccountCircle from '@material-ui/icons/AccountCircle';
@@ -76,22 +75,10 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
   },
 
-  // sectionDesktop: {
-  //   display: 'none',
-  //   [theme.breakpoints.up('md')]: {
-  //     display: 'flex',
-  //   },
-  // },
-  // sectionMobile: {
-  //   display: 'flex',
-  //   [theme.breakpoints.up('md')]: {
-  //     display: 'none',
-  //   },
-  // },
 }));
 
 
-function ToolBar({data, showList=false, indexes, onSearch, buttons=null}) {
+function ToolBar({data, showList=false, indexes, onSearch, buttons=null, placeholder="Search..."}) {
 
   const classes = useStyles();
   const [anchorEl, setAnchorEl] = useState(null);
@@ -150,72 +137,6 @@ const handleSearch = (e) => {
 }
 
 
-/**
- * 
- *  
-*/
-
-
-  const handleProfileMenuOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMobileMenuClose = () => {
-    setMobileMoreAnchorEl(null);
-  };
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-    handleMobileMenuClose();
-  };
-
-  const handleMobileMenuOpen = (event) => {
-    setMobileMoreAnchorEl(event.currentTarget);
-  };
-
-
- 
-  const menuId = 'primary-search-account-menu';
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-    </Menu>
-  );
-
-  const mobileMenuId = 'primary-search-account-menu-mobile';
-  const renderMobileMenu = (
-    <Menu
-      anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={isMobileMenuOpen}
-      onClose={handleMobileMenuClose}
-    >
-      <MenuItem>
-        <Button aria-label="show 4 new mails" color="inherit">
-         test
-        </Button>
-      </MenuItem>
-      <MenuItem>
-        <Button aria-label="show 11 new notifications" color="inherit">
-            srest   
-        </Button>
-     
-      </MenuItem>
-     
-    </Menu>
-  );
 
   return (
     <>
@@ -238,7 +159,7 @@ const handleSearch = (e) => {
               <SearchIcon />
             </div>
             <InputBase
-              placeholder="Search…"
+              placeholder={placeholder}
               classes={{
                 root: classes.inputRoot,
                 input: classes.inputInput,
@@ -267,8 +188,7 @@ const handleSearch = (e) => {
           </div> */}
         </Toolbar>
       </AppBar>
-      {renderMobileMenu}
-      {renderMenu}
+  
     </div>
     </>
   );
