d1dce61db0e0a13e240a78166cbed200b93efef6
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-03-28 13:59:05 +0200

    changed people icon to chat

--- src/views/people/index.js
+++ src/views/people/index.js
@@ -1,4 +1,4 @@
 export {default as PeopleList} from './PeopleList'
 export {default as PeopleCreate} from './PeopleCreate'
 export {default as PeopleEdit} from './PeopleEdit'
-export {default as PeopleIcon} from '@material-ui/icons/Group';
+export {default as PeopleIcon} from '@material-ui/icons/Chat';
