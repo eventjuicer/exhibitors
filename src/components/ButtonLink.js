62f72d15d60aad7a186e2080df9471c566ba1400
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-03-30 13:32:07 +0200

    do not show label when isMobile and startIcon is set

--- src/components/ButtonLink.js
+++ src/components/ButtonLink.js
@@ -1,9 +1,17 @@
 
 import Button from './Button'
 import { useHistory } from 'react-router-dom';
+import { useIsMobile } from '../helpers';
+
 
 const ButtonLink = ({to, query={}, label="common.details", ...buttonProps}) => {
+    const isMobile = useIsMobile("md")
     const {push} = useHistory()
+
+    if(isMobile && "startIcon" in buttonProps){
+        label = ""
+    }
+
     return <Button onClick={() => push({
         pathname: to,
         search: new URLSearchParams(query).toString()
