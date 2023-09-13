72661c23d8a09ca44fdb859bb38e7a12caa132fc
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-09-13 13:14:28 +0200

    show arrangement

--- src/settings.js
+++ src/settings.js
@@ -187,7 +187,7 @@ const settings = {
     {name: "event", icon: BuildIcon, children: [
       {name: "logistics", visible: true}, 
       {name: "representatives", visible: false}, 
-      {name: "arrangement", visible: false}, 
+      {name: "arrangement", visible: true}, 
       {name: "vouchers", visible: false}, 
       {name: "register/party", visible: false}, 
     ]},
