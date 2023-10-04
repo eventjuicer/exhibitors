c3ae3ae9308e19fc0f84b92d484a7f64aae79a76
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-10-04 12:05:30 +0200

    enabled reps

--- src/settings.js
+++ src/settings.js
@@ -186,7 +186,7 @@ const settings = {
     ]},
     {name: "event", icon: BuildIcon, children: [
       {name: "logistics", visible: true}, 
-      {name: "representatives", visible: false}, 
+      {name: "representatives", visible: true}, 
       {name: "arrangement", visible: true}, 
       {name: "vouchers", visible: false}, 
       {name: "register/party", visible: false}, 
