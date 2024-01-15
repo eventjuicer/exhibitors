689d75ade8dbfb102f3e4d2863e62136412e76e1
Adam Zygadlewicz <adam@zygadlewicz.com>
2024-01-15 12:51:02 +0100

    resolves https://github.com/ecommerceberlin/exhibitors/issues/156

--- src/views/representatives/RepresentativeCreate.js
+++ src/views/representatives/RepresentativeCreate.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import {
   Create,
-  SimpleForm
+  SimpleForm,
+  Link,
+  useTranslate
 } from 'react-admin';
 
 import inputs from './inputs'
+import { useCompanyDataItem } from '../../helpers';
+import { isEmpty } from '../../helpers';
+import { Alert, Box } from '../../components';
 
-const RepresentativeCreate = props => (
-  <Create {...props}>
-    <SimpleForm redirect="list" submitOnEnter={false}>
-      {inputs}
-    </SimpleForm>
-  </Create>
-);
 
+const RepresentativeCreate = props => {
+
+  const name = useCompanyDataItem("name")
+  const translate = useTranslate();
+
+  if(isEmpty(name)){
+
+    return (<Box m={4}><Alert titleLabel="errors.please_add_company_name">
+        <Link to={{ pathname: '/companydata' }}>{translate('ra.action.edit')}</Link>
+      </Alert></Box>)
+
+  }
+
+  return (
+    <Create {...props}>
+      <SimpleForm redirect="list" submitOnEnter={false}>
+        {inputs}
+      </SimpleForm>
+    </Create>
+  );
+
+  
+}
 export default RepresentativeCreate;
