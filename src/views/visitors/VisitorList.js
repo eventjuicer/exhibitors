58eb8152107a703762889bff15086dfb5b2590ca
Adam Zygadlewicz <adam@zygadlewicz.com>
2024-01-18 14:42:07 +0100

    added filtering by company_role
    related https://github.com/ecommerceberlin/exhibitors/issues/177

--- src/views/visitors/VisitorList.js
+++ src/views/visitors/VisitorList.js
@@ -19,8 +19,22 @@ const participant_types = [
     "student"
 ]
 
+const company_roles = [
+  "student",
+  "entry",
+  "manager",
+  "professional",
+  "head_of_department",
+  "director",
+  "c_level",
+  "board_member",
+  "other"
+]
+
 
 const filters = [
+
+
 
   <SelectInput
   source="participant_type"
@@ -28,6 +42,14 @@ const filters = [
   choices={participant_types.map(item => ({id: item, name: `resources.visitors.fields.participant_type.options.${item}`}))}
   alwaysOn
   />,
+
+  <SelectInput
+  source="company_role"
+  allowEmpty
+  choices={company_roles.map(item => ({id: item, name: `resources.visitors.fields.company_role.options.${item}`}))}
+  alwaysOn
+  />,
+  
 
   <TextInput label="pos.search" source="q" alwaysOn />
 ]
