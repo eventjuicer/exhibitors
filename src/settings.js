d1ab8c343d5f9c8996e8c4a267afb3293f317e4c
Adam Zygadlewicz <adam@zygadlewicz.com>
2024-03-25 11:30:21 +0100

    enabled vips and visitors

--- src/settings.js
+++ src/settings.js
@@ -194,9 +194,9 @@ const settings = {
     ]},
 
     {name: "leads", icon: RecentActorsIcon, children: [
-      {name: "visitors", visible: false}, 
+      {name: "visitors", visible: true}, 
       {name: "meetups", visible: true}, 
-      {name: "vips", visible: false}, 
+      {name: "vips", visible: true}, 
       {name: "scans", visible: false}, 
     ]},
 
