ecd6f80c93c0534bc7816940aaf2280e5a78a712
Adam Zygadlewicz <adam@zygadlewicz.com>
2024-10-15 12:13:41 +0200

    enabled visitors

--- src/settings.js
+++ src/settings.js
@@ -194,7 +194,7 @@ const settings = {
     ]},
 
     {name: "leads", icon: RecentActorsIcon, children: [
-      {name: "visitors", visible: false}, 
+      {name: "visitors", visible: true}, 
       {name: "meetups", visible: true}, 
       {name: "vips", visible: true}, 
       {name: "scans", visible: false}, 
