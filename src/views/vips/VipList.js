9df6022ffecad2bb85836f458d38e022f4ea2937
Adam Zygadlewicz <adam@zygadlewicz.com>
2025-03-24 14:13:53 +0100

    Update VipList.js

--- src/views/vips/VipList.js
+++ src/views/vips/VipList.js
@@ -8,7 +8,8 @@ import { VipIcon } from '.'
 import jsonExport from 'jsonexport/dist';
 import { useCompany } from '../../contexts';
 import { vipCodeUrl } from '../../helpers';
-
+import {useTranslate} from 'react-admin';
+import { Link } from 'react-router-dom';
 
 const Aside = (props) => (
   <ResourceAbout 
@@ -48,33 +49,70 @@ const exporter = (slug) => (items) => {
 };
 
 
-const VipList = props => {
 
-  const slug = useCompany("slug")
+
+export default function VisitorListMoved({basePath = ''}) {
+  const translate = useTranslate()
 
   return (
-    <List
-      {...props}
-      // filters={<VisitorFilter />}
-      sort={{ field: 'lname', order: 'ASC' }}
-      perPage={100}
-      title={ <ResourceTitle {...props} />}
-      exporter={ exporter(slug) }
-      aside={ <Aside {...props} />}
-      bulkActionButtons={false}
-  >
-      <Datagrid>
-  
-        <TextFieldShort source="code" limit="10" sortable={false} />
-        <IsVipcodeSent label="fields.recipient" source="email" />
-        <IsVipcodeUsed source="participant" />
-        <UseVipcodeButton />
-        
-      </Datagrid>
-    </List>
-  );
-
+    <div style={{ 
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      padding: '20px',
+    }}>
+      <h2 style={{marginBottom: '20px'}}>{translate('resources.vips.wtf-happened-here.title')}</h2>
+      <p>{translate('resources.vips.wtf-happened-here.description')} <Link to="/representatives">{translate('resources.vips.wtf-happened-here.link')}</Link></p>
+      
+      <div style={{ width: '70%', marginTop: '20px', paddingBottom: '100px'}}>
+        <img 
+          src={'/app_access.png'} 
+          alt="visitors-moved" 
+          style={{
+            width: '100%',
+            height: '100%',
+            objectFit: 'contain'
+          }}
+        />
+      </div>
+    </div>
+  )
 }
 
-export default VipList;
+
+
+
+
+
+// const VipList = props => {
+
+//   const slug = useCompany("slug")
+
+//   return (
+//     <List
+//       {...props}
+//       // filters={<VisitorFilter />}
+//       sort={{ field: 'lname', order: 'ASC' }}
+//       perPage={100}
+//       title={ <ResourceTitle {...props} />}
+//       exporter={ exporter(slug) }
+//       aside={ <Aside {...props} />}
+//       bulkActionButtons={false}
+//   >
+//       <Datagrid>
+  
+//         <TextFieldShort source="code" limit="10" sortable={false} />
+//         <IsVipcodeSent label="fields.recipient" source="email" />
+//         <IsVipcodeUsed source="participant" />
+//         <UseVipcodeButton />
+        
+//       </Datagrid>
+//     </List>
+//   );
+
+// }
+
+ 
 
